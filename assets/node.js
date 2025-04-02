69e015c7a6f856940ef688b9e12ba3aa6d69d03f
_r.lucz_ <165353824+lu241112003@users.noreply.github.com>
2025-04-02 20:55:38 -0300

    Add files via upload

--- assets/node.js
+++ assets/node.js
@@ -1,20 +1,20 @@
 // Learn More Button
 document.getElementById("learnMore").addEventListener("click", function() {
-    alert("Obrigado por querer saber mais!");
+  alert("Obrigado por querer saber mais!");
 });
 
 // Download CV
 function baixarCurriculo() {
-    const url = "https://drive.google.com/uc?export=download&id=1QjQ25i3nvMjlf5sRm1fd7CjnscSbCiyQ";
-    window.open(url, "_blank");
+  const url = "https://drive.google.com/uc?export=download&id=1QjQ25i3nvMjlf5sRm1fd7CjnscSbCiyQ";
+  window.open(url, "_blank");
 }
 
 // Open WhatsApp
 function abrirWhatsApp() {
-    const numero = "+5541987828512";
-    const mensagem = "Olá Ronald, gostaria de trabalhar com você!";
-    const url = `https://wa.me/${numero.replace(/\D/g, "")}?text=${encodeURIComponent(mensagem)}`;
-    window.open(url, "_blank");
+  const numero = "+5541987828512";
+  const mensagem = "Olá Ronald, gostaria de trabalhar com você!";
+  const url = `https://wa.me/${numero.replace(/\D/g, "")}?text=${encodeURIComponent(mensagem)}`;
+  window.open(url, "_blank");
 }
 
 // Carousel Logic
@@ -25,16 +25,16 @@ const nextBtn = document.querySelector('.next');
 let currentIndex = 0;
 
 function updateCarousel() {
-    carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
-    carouselInner.style.transition = 'transform 0.5s ease';
+  carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
+  carouselInner.style.transition = 'transform 0.5s ease';
 }
 
 prevBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex > 0) ? currentIndex - 1 : carouselItems.length - 1;
-    updateCarousel();
+  currentIndex = (currentIndex > 0) ? currentIndex - 1 : carouselItems.length - 1;
+  updateCarousel();
 });
 
 nextBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex < carouselItems.length - 1) ? currentIndex + 1 : 0;
-    updateCarousel();
+  currentIndex = (currentIndex < carouselItems.length - 1) ? currentIndex + 1 : 0;
+  updateCarousel();
 });
